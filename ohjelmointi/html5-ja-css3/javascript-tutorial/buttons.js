6b008f369720fb0d583f1c77f02101392128b949
Anu Virtanen <s2000604@edu.bc.fi>
2020-10-09 14:17:00 +0300

    Javascript tutoriaalisivu valmis

--- ohjelmointi/html5-ja-css3/javascript-tutorial/buttons.js
+++ ohjelmointi/html5-ja-css3/javascript-tutorial/buttons.js
@@ -44,18 +44,11 @@ patienceButton.addEventListener("click", function() {
 //find jstutorialWindowBtn
 let jstutorialWindowBtn = document.getElementById("jstutorialWindowBtn");
 
-//define window opening settings 
 
-/*let features = 'height=600,width=800',
-    url = 'https://www.javascripttutorial.net/javascript-bom/javascript-window/';
-
-let jsWindow = window.open(url, 'about', features); */
-
-
-
-//add click function to jstutorialWindowBtn 
+//add click function to jstutorialWindowBtn: open new window, first javascripttutorial.net and then after 3 s this page
 jstutorialWindowBtn.addEventListener("click", function() {
   
+  //define window opening settings 
     let features = 'height=600,width=800',
     url = 'https://www.javascripttutorial.net/javascript-bom/javascript-window/';
 
@@ -65,6 +58,50 @@ let jsWindow = window.open(url, 'about', features);
         window.open('https://intra.bc.fi/s2000604/portfolio/ohjelmointi/html5-ja-css3/javascript-tutorial/', 'about')
     }, 3000);
 
+
+});
+
+//find jstutorialPromptBtn and add click counter
+let jstutorialPromptBtn = document.getElementById("jstutorialPromptBtn");
+let clickCounter = 0;
+
+//add click function to jstutorialPromptBtn: prompt
+
+jstutorialPromptBtn.addEventListener("click", function() {
+  clickCounter++;
+  if (clickCounter === 1) {
+    let result = window.prompt("Voit kirjoittaa tekstikenttään, mutta se ei tee mitään.");
+  }
+  if (clickCounter ===2) {
+    let answer = prompt('Karaoke vai hiljaisuus?');
+
+    let feedback = answer.toLowerCase() === '' ? '' :
+    `${answer}! Oijoi.`;
+
+    alert(feedback);
+    clickCounter = 0;
+
+
+  };
+
+  //setTimeout -buttons have their event listeners attached in HTML. Below only setTimeout
+
+  let showBtn = document.getElementById("showBtn");
+  let cancelBtn = document.getElementById("cancelBtn");
+
+  var timeoutID;
+
+  showBtn.addEventListener("click", function() {
+    timeoutID = setTimeout(alert, 3000, 'Tämä tervehdys tuli 3 sekunnin viiveellä.');
+  });
+
+ 
+
+ cancelBtn.addEventListener("click", function () {
+    clearTimeout(timeoutID);
+  });
+    
   
  
 });
+
